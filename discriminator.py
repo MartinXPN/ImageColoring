3e2e6d77f0744e7540dbb44f440ea7d604150587
MartinXPN <mirakyanmartin@gmail.com>
2017-07-19 02:28:36 +0400

    added leaky relus and batch normalizations to discriminator

--- discriminator.py
+++ discriminator.py
@@ -2,40 +2,81 @@ def create_discriminator(input_shape=(224, 224, 3)):
     
     from keras.models import Model
     from keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense
+    from keras.layers.advanced_activations import LeakyReLU
+    from keras.layers.normalization import BatchNormalization
 
     
     inputs = Input(shape=input_shape)
     
     # Block 1
-    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(inputs)
-    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+    x = Conv2D(64, (3, 3), activation=None, padding='valid')(inputs)
+    x = LeakyReLU()(x)
+    
+    x = Conv2D(64, (3, 3), strides=(2, 2), activation=None, padding='valid')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU()(x)
 
+    
     # Block 2
-    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
-    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+    x = Conv2D(128, (3, 3), activation=None, padding='valid')(x)
+    x = LeakyReLU()(x)
+    
+    x = Conv2D(128, (3, 3), strides=(2, 2), activation=None, padding='valid')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU()(x)
 
+    
     # Block 3
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+    x = Conv2D(256, (3, 3), activation=None, padding='valid')(x)
+    x = LeakyReLU()(x)
+    
+    x = Conv2D(256, (3, 3), activation=None, padding='valid')(x)
+    x = LeakyReLU()(x)
+    
+    x = Conv2D(256, (3, 3), strides=(2, 2), activation=None, padding='valid')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU()(x)
 
+    
     # Block 4
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+    x = Conv2D(512, (3, 3), activation=None, padding='valid')(x)
+    x = LeakyReLU()(x)
+    
+    x = Conv2D(512, (3, 3), activation=None, padding='valid')(x)
+    x = LeakyReLU()(x)
+    
+    x = Conv2D(512, (3, 3), activation=None, padding='valid')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU()(x)
 
+    
     # Block 5
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
-    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+    x = Conv2D(512, (3, 3), activation=None, padding='valid')(x)
+    x = LeakyReLU()(x)
+    
+    x = Conv2D(256, (3, 3), activation=None, padding='valid')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU()(x)
+    
+    x = Conv2D(256, (3, 3), activation=None, padding='valid')(x)
+    x = LeakyReLU()(x)
+    
+    x = Conv2D(128, (3, 3), activation=None, padding='valid')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU()(x)
+    
+    x = Conv2D(128, (3, 3), activation=None, padding='valid')(x)
+    x = LeakyReLU()(x)
+    
+    x = Conv2D(64, (3, 3), activation=None, padding='valid')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU()(x)
 
+    
+    
     x = Flatten(name='flatten')(x)
-    x = Dense(512, activation='relu', name='fc1')(x)
+    x = Dense(512, activation=None, name='fc1')(x)
+    x = LeakyReLU()(x)
     out = Dense(1, activation='sigmoid', name='out')(x)
     
     model = Model(inputs, out, name='discriminator')
