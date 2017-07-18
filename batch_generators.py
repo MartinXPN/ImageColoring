3ac91819f246a101579ee03a78b2d413a81e74e6
MartinXPN <mirakyanmartin@gmail.com>
2017-07-19 00:51:59 +0400

    started training with new batch generators and visualization

--- batch_generators.py
+++ batch_generators.py
@@ -1,8 +1,16 @@
 import random
 import numpy as np
 import threading
+from keras.preprocessing.image import load_img
+from skimage import io, color
+from skimage.transform import rescale, resize, downscale_local_mean
 
-class BaseBatchGenerator:
+
+image_mean = (103.939, 116.779, 123.68)
+greyscale_image_mean = np.mean(image_mean)
+
+
+class BaseBatchGenerator(object):
     
     def __init__(self, image_paths, batch_size, image_height, image_width, shuffle):
         self.image_paths = image_paths
@@ -16,32 +24,35 @@ class BaseBatchGenerator:
 
     def next(self):
         with self.lock:
-            batch_features = []
+            batch_inputs = []
             batch_labels   = []
 
             for i in range(self.batch_size):
                 path = self.image_paths[self.index]
-                x, y = generate_one(path)
+                x, y = self.generate_one(path)
+                
+                batch_inputs.append( x )
+                batch_labels.append( y )
 
                 self.index += 1
                 if self.index >= len(self.image_paths):
                     self.index = 0
             
-            return np.array(batch_features), np.array(batch_labels)
+            return np.array(batch_inputs), np.array(batch_labels)
     
-    def generate_one(path):
+    def generate_one(self, path):
         raise NotImplementedError("Please Implement this method")
 
 
-class DiscriminatorBatchGenerator(BaseBatchGenerator):
-    """"
+class DiscriminatorRealGenerator(BaseBatchGenerator):
+    '''
     Generates real images to make the discriminator better in predicting real images
-    """"
+    '''
     
     def __init__(self, image_paths, batch_size, image_height, image_width, shuffle = True):
-        super(DiscriminatorBatchGenerator, self).__init__(image_paths, batch_size, image_height, image_width, shuffle)
+        super(DiscriminatorRealGenerator, self).__init__(image_paths, batch_size, image_height, image_width, shuffle)
         
-    def generate_one(path):
+    def generate_one(self, path):
         image_gray = load_img(path, grayscale=True, target_size=(self.image_height, self.image_width))
         x = np.zeros((self.image_height, self.image_width, 3))
         x[:,:,0] = image_gray - greyscale_image_mean
@@ -57,28 +68,28 @@ class ColorizerBatchGenerator(BaseBatchGenerator):
     Generates input for colorizer to improve it's performance in fooling the discriminator
     """
     def __init__(self, image_paths, batch_size, image_height, image_width, shuffle = True):
-        super(DiscriminatorBatchGenerator, self).__init__(image_paths, batch_size, image_height, image_width, shuffle)
+        super(ColorizerBatchGenerator, self).__init__(image_paths, batch_size, image_height, image_width, shuffle)
 
         
-    def generate_one(path):
+    def generate_one(self, path):
         image = io.imread(path)
         image = resize(image, output_shape=(self.image_height, self.image_width, 3))
         image_gray = load_img(path, grayscale=True, target_size=(self.image_height, self.image_width))
         x = color.rgb2lab(image)
         x[:,:,0] = image_gray - greyscale_image_mean
-        y = get_label()
+        y = self.get_label()
         
         return x, y
    
-    def get_label():
+    def get_label(self):
         return True
     
-class DiscriminatorBatchGeneratorForColorizer(ColorizerBatchGenerator):
+class DiscriminatorFakeGenerator(ColorizerBatchGenerator):
     """
     Generates input for colorizer to improve discriminator's performance in detecting fake images
     """
     def __init__(self, image_paths, batch_size, image_height, image_width, shuffle = True):
-        super(DiscriminatorBatchGeneratorForColorizer, self).__init__(image_paths, batch_size, image_height, image_width, shuffle)
+        super(DiscriminatorFakeGenerator, self).__init__(image_paths, batch_size, image_height, image_width, shuffle)
 
-    def get_label():
+    def get_label(self):
         return False
