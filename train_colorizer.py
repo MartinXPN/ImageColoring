2a69875b6786e146972cfcbb022a6cae8816e5b5
MartinXPN <mirakyanmartin@gmail.com>
2018-05-07 11:09:59 +0400

    created helper function to initialize class weights and color space mapping

--- train_colorizer.py
+++ train_colorizer.py
@@ -11,8 +11,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from scipy.misc import imsave
 
 from models.colorizer import get_colorizer
-from util.colorspace.mapping import get_mapper
-from util.colorspace.colorspaceclasses import ColorMappingInitializer, ColorFrequencyCalculator
+from util.colorspace.initialize import get_mapping_with_class_weights
 
 
 class Gym(object):
@@ -74,23 +73,9 @@ def main(batch_size=32, image_size=224, epochs=100000, steps_per_epoch=100, colo
                                                                batch_size=batch_size,
                                                                color_mode='rgb',
                                                                class_mode=None)
-    class_weights = None
-    if classifier:
-        mapping = ColorMappingInitializer(scale_factor=scale_factor)
-        mapping.initialize()
-        data_mapper = get_mapper(color_space=color_space, classifier=classifier,
-                                 color_to_class=mapping.color_to_class, class_to_color=mapping.class_to_color,
-                                 factor=mapping.scale_factor)
-        class_weight_calculator = ColorFrequencyCalculator(color_to_class=mapping.color_to_class,
-                                                           class_to_color=mapping.class_to_color,
-                                                           rgb_image_to_classes=data_mapper.rgb_to_classes,
-                                                           image_generator=train_generator,
-                                                           image_size=image_size)
-        class_weight_calculator.populate(num_batches=populate_batches)
-        class_weights = class_weight_calculator.get_class_weights()
-    else:
-        data_mapper = get_mapper(color_space=color_space, classifier=classifier)
-
+    data_mapper, class_weights = get_mapping_with_class_weights(classifier=classifier, color_space=color_space,
+                                                                image_generator=train_generator, image_size=image_size,
+                                                                nb_batches=populate_batches, scale_factor=scale_factor)
     ''' Prepare Models '''
     colorizer = get_colorizer(image_size=image_size, vgg=vgg, feature_extractor_model_path=feature_extractor_model_path,
                               train_feature_extractor=train_feature_extractor,
